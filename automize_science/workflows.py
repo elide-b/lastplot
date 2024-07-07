e66aa91f1703292fa132a8565eef907fde40bdbb
Elide <elide.98@gmail.com>
2024-07-07 19:06:53 -0400

    last improvements

--- automize_science/workflows.py
+++ automize_science/workflows.py
@@ -1,10 +1,9 @@
 from automize_science.computing_statistics import *
 from automize_science.data_cleanup import *
-from automize_science.graph_constructor import *
 from automize_science.saving import *
 
 
-def workflow(file_path, data_sheet, mice_sheet, output_path, control_name, experimental_name, palette="Set2"):
+def data_workflow(file_path, data_sheet, mice_sheet, output_path, control_name):
     """
     Automatically processes lipidomics data.
 
@@ -13,19 +12,16 @@ def workflow(file_path, data_sheet, mice_sheet, output_path, control_name, exper
     :param mice_sheet: Name of the sheet containing the information about the subjects.
     :param output_path: Path of where to save the outputs.
     :param control_name: Name of the control subject group.
-    :param experimental_name: Name of the experimental subject group.
-    :param palette: Color of the graphs.
     """
 
     df, df_mice = load_data(datapath=file_path, sheet_name=data_sheet, mice_sheet=mice_sheet)
     df_clean = data_cleanup(df=df, df_mice=df_mice, output_path=output_path)
     statistics = statistics_tests(df_clean=df_clean, control_name=control_name)
-
     df_final = z_scores(df_clean=df_clean, statistics=statistics)
     save_values(df_final=df_final, output_path=output_path)
     save_zscores(df_final=df_final, output_path=output_path)
     wb = load_workbook(output_path + "/output/Output file.xlsx")
     wb.create_sheet(title="Comments")
     wb.save(output_path + "/output/Output file.xlsx")
-    zscore_graph_lipid(df_final, control_name, experimental_name, output_path=output_path, palette=palette)
-    zscore_graph_lipid_class(df_final, control_name, experimental_name, output_path=output_path, palette=palette)
+
+    return df_final
--- example/example.py
+++ example/example.py
@@ -1,4 +1,5 @@
-from automize_science.script import *
+from automize_science.graph_constructor import *
+from automize_science.workflows import *
 
 file_path = "Dementia project.xlsx"
 data_sheet = "Quantification"
@@ -9,4 +10,29 @@ experimental_name = "FTLD"
 palette = "Set2"
 
 
-workflow(file_path, data_sheet, mice_sheet, output_path, control_name, experimental_name, palette)
+df = data_workflow(
+    file_path="Dementia project.xlsx",
+    data_sheet="Quantification",
+    mice_sheet="Sheet1",
+    output_path="C:/Users/Elide/Documents/git/automize-science/example",
+    control_name="WT",
+)
+
+zscore_graph_lipid(
+    df_final=df,
+    control_name="WT",
+    experimental_name="FTLD",
+    output_path="C:/Users/Elide/Documents/git/automize-science/example",
+    palette="Set2",
+    show=True,
+)
+
+
+zscore_graph_lipid_class(
+    df_final=df,
+    control_name="WT",
+    experimental_name="FTLD",
+    output_path="C:/Users/Elide/Documents/git/automize-science/example",
+    palette="Set2",
+    show=True,
+)

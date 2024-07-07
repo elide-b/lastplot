1706e2f62f74bb161ab76e0d4fa23459a97b18ad
Elide <elide.98@gmail.com>
2024-07-07 19:12:11 -0400

    first release

--- automize_science/__init__.py
+++ automize_science/__init__.py
@@ -1,5 +1,7 @@
 """
-This is pipeline to blah blah blah
+Automize Science is a Python package designed to elaborate data into graphs coming from lipid extractions (LC/MS).
+Starting from a file containing the **pmol/mg** values per each sample, this package streamlines the process of data
+analysis and visualization.
 """
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'

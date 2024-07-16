6b77cd2bff8312f8bf3c3c1ffe7127a9dae7b2f4
Elide <elide.98@gmail.com>
2024-07-16 10:27:22 -0400

    test for the data

---
+++ example/test.py
@@ -0,0 +1,46 @@
+import lastplot
+
+df = lastplot.data_workflow(
+    file_path="Dementia project.xlsx",
+    data_sheet="Quantification",
+    mice_sheet="Sheet1",
+    output_path="./example",
+    control_name="WT",
+    experimental_name=["FTLD"],
+)
+#
+# lastplot.zscore_graph_lipid(
+#     df,
+#     control_name="WT",
+#     experimental_name=["FTLD"],
+#     output_path="./example",
+#     palette="Set3",
+#     show=False
+# )
+#
+# lastplot.zscore_graph_lipid_class(
+#     df,
+#     control_name="WT",
+#     experimental_name=["FTLD"],
+#     output_path="./example",
+#     palette="Set3",
+#     show=False
+# )
+#
+# lastplot.log_values_graph_lipid(
+#     df,
+#     control_name="WT",
+#     experimental_name=["FTLD"],
+#     output_path="./example",
+#     palette="Set3",
+#     show=False
+# )
+
+lastplot.log_values_graph_lipid_class(
+    df,
+    control_name="WT",
+    experimental_name=["FTLD"],
+    output_path="./example",
+    palette="Set3",
+    show=False,
+)
